6f1de5f2df100f0a5c624658ed7728151d564111
Nanda Syahrasyad <nanda.s@hey.com>
2021-12-25 11:03:39 -0800

    feat(cookies): move index to login and add comments

--- cookies/README.md
+++ cookies/README.md
@@ -14,3 +14,15 @@ deno --allow-net --allow-read server.ts
 3. Open up your browser and navigate to `localhost:8000`
 4. In the username and password form, type in `hello` as the username and `world` as the password
 5. You should be redirected to the welcome page, and if you look in the network tab, you should see a new cookie was sent in the request
+
+## Why Deno?
+
+I knew I needed to write my own HTTP server for this but I _also_ knew I wanted to:
+
+1. Use Typescript
+2. Use as few dependencies as possible
+3. Use the fetch API's interfaces if possible
+
+It just so happens that Deno's HTTP standard library ticks all of these boxes out of the box.
+
+## How does it work?
--- cookies/server.ts
+++ cookies/server.ts
@@ -4,7 +4,15 @@ const BASE_URL = "http://localhost:8000";
 const USERNAME = "hello";
 const PASSWORD = "world";
 
-function parseCookies(cookies: string) {
+/**
+ * Parses the cookies string into key-value pairs.
+ * e.g. given the string "logged-in=true; user=hello",
+ *  returns the object { 'logged-in': 'true', user: 'hello' }
+ */
+function parseCookies(cookies: string | null) {
+  if (!cookies) {
+    return {};
+  }
   return cookies
     .split(";")
     .map((pair) => pair.trim())
@@ -17,58 +25,85 @@ function parseCookies(cookies: string) {
     }, {} as Record<string, string>);
 }
 
-function isLoggedIn(cookies: string | null) {
-  if (!cookies) {
-    return false;
-  }
-  return parseCookies(cookies)["logged-in"] === "true";
+/**
+ * A redirect response is a response code with a 3xx code. When the browser
+ * receives a 3xx code, it looks for the `Location` field in the response
+ * headers to determine where it should redirect to.
+ *
+ * "Location" can be either a relative or absolute path.
+ */
+function redirect(to: string, headers: Record<string, string> = {}) {
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: to,
+      ...headers,
+    },
+  });
+}
+
+async function html(path: string, headers: Record<string, string> = {}) {
+  const html = await Deno.readTextFile(path + ".html");
+  return new Response(html, {
+    headers: {
+      "Content-Type": "text/html",
+      ...headers,
+    },
+  });
 }
 
 async function handler(req: Request): Promise<Response> {
   const [, path] = req.url.split(BASE_URL);
+  const cookies = parseCookies(req.headers.get("Cookie"));
 
+  /**
+   * Checks whether the user is logged in by checking if the
+   * request cookies contains the cookie logged-in=true
+   */
+  function isLoggedIn() {
+    return cookies["logged-in"] === "true";
+  }
+
+  /**
+   * The root handler. If the user is logged in, redirects to the home page.
+   * Otherwise it redirects to the login page.
+   */
   if (req.method === "GET" && path === "/") {
-    const html = await Deno.readTextFile("./index.html");
-    return new Response(html, {
-      headers: {
-        "Content-Type": "text/html",
-        "Set-Cookie": `logged-in=''; expires=${new Date(
-          0
-        ).toUTCString()}; HttpOnly;`,
-      },
-    });
-  }
-
-  if (req.method === "GET" && path === "/home") {
-    if (!isLoggedIn(req.headers.get("Cookie"))) {
-      return new Response(null, {
-        status: 302,
-        headers: {
-          Location: "./",
-        },
-      });
+    if (isLoggedIn()) {
+      return redirect("./home");
+    } else {
+      return redirect("./login");
     }
+  }
 
-    const html = await Deno.readTextFile("./home.html");
-    return new Response(html, {
-      headers: {
-        "Content-Type": "text/html",
-      },
+  /**
+   * Returns the login page and deletes the 'logged-in' cookie. The correct way
+   * to delete a cookie is to set the cookie value to an empty string and set
+   * its expiration to a date in the past.
+   *
+   * See:
+   *  - https://stackoverflow.com/questions/5285940/correct-way-to-delete-cookies-server-side
+   */
+  if (req.method === "GET" && path === "/login") {
+    return html("./login", {
+      "Set-Cookie": `logged-in=''; expires=${new Date(
+        0
+      ).toUTCString()}; HttpOnly;`,
     });
   }
 
+  /**
+   * Handler for authentication. Checks if the username and password matches
+   * and if it does, redirects to the home page and sets the logged in cookie.
+   */
   if (req.method === "POST" && path === "/login") {
     const formData = await req.formData();
     const username = formData.get("username");
     const password = formData.get("password");
 
     if (username === USERNAME && password === PASSWORD) {
-      return new Response(null, {
-        status: 302,
-        headers: {
-          Location: "../home",
-          "Set-Cookie": "logged-in=true; HttpOnly",
-        },
+      return redirect("../home", {
+        "Set-Cookie": "logged-in=true; HttpOnly",
       });
     } else {
       return new Response("Incorrect username or password", {
@@ -77,6 +112,25 @@ async function handler(req: Request): Promise<Response> {
     }
   }
 
+  /**
+   * If the user is logged in (i.e. the cookie exists), return the login page.
+   * Otherwise redirect the user to the login form.
+   *
+   * I thought about returning a 403 instead of a redirect but it seems that
+   * many large websites simply do a 302 (Facebook, Google, Amazon). See:
+   *  - https://stackoverflow.com/questions/8775593/is-it-possible-to-send-a-401-unauthorized-and-redirect-with-a-location
+   */
+  if (req.method === "GET" && path === "/home") {
+    if (isLoggedIn()) {
+      return html("./home");
+    }
+    return redirect("./login");
+  }
+
+  /**
+   * At this point, we're trying to handle a route that doesn't exist, so we
+   * return a 404.
+   */
   return new Response("Not found", {
     status: 404,
   });
